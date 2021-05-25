a4d545f6ad4c75b09da53a196b3d9bfdb0e7683c
amirazak11 <amirazak1032000@gmail.com>
2021-05-25 04:43:17 +0200

    first commit

---
+++ home/java.js
@@ -0,0 +1,12 @@
+function showmsg(){
+        document.getElementById("mymsg").style.display = "block";
+
+    }
+
+function hidemsg(){
+        document.getElementById("mymsg").style.display = "none";
+
+
+  
+
+}
